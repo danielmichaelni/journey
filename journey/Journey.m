784d8df04b06d4d00301ba9d599f5507ff38fd0e
Daniel Ni <danielmichaelni@gmail.com>
2015-02-28 01:46:40 -0600

    basic timer functionality

---
+++ journey/Journey.m
@@ -0,0 +1,20 @@
+//
+//  Journey.m
+//  journey
+//
+//  Created by Daniel Ni on 2/27/15.
+//  Copyright (c) 2015 charles. All rights reserved.
+//
+
+#import "Journey.h"
+
+@implementation Journey
+
+- (id) initWithDestination:(GMSCameraPosition *)destination andTimer:(NSTimer *)timer {
+    self.destination = destination;
+    self.timer = timer;
+    
+    return self;
+}
+
+@end
