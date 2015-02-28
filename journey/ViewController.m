8f7e6f28c8f05110ccf0f47781763d240b1bb1cb
Charles Chamberlain <cchamberlain@uchicago.edu>
2015-02-28 01:55:40 -0600

    revert

--- journey/ViewController.m
+++ journey/ViewController.m
@@ -21,7 +21,7 @@
     [super viewDidLoad];
     
     // Create a GMSCameraPosition that tells the map to display the
-    // coordinate -33.86,151.20 at zoom level 6.
+    // coordinate -33.86,151.20 at zoom level 6.c
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
                                                             longitude:151.20
                                                                  zoom:6];
