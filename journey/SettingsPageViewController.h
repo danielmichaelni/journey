ab829be013de312b9a19bf6eca9ed3d7c336cfd5
Sun77789 <aubakirova.m.m@gmail.com>
2015-02-28 18:47:29 -0600

    Settings page

---
+++ journey/SettingsPageViewController.h
@@ -0,0 +1,25 @@
+//
+//  SettingsPageViewController.h
+//  journey
+//
+//  Created by Malika Aubakirova on 2/28/15.
+//  Copyright (c) 2015 charles. All rights reserved.
+//
+
+#ifndef journey_SettingsPageViewController_h
+#define journey_SettingsPageViewController_h
+#import <UIKit/UIKit.h>
+
+@interface SettingsPageViewController :UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *profilePhoto;
+
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *saveChanges;
+@property (strong, nonatomic) IBOutlet UITextField *updatePhone;
+@property (strong, nonatomic) IBOutlet UITextField *updateEmail;
+@property (strong, nonatomic) IBOutlet UIButton *changeFriendsList;
+@property (strong, nonatomic) IBOutlet UISwitch *enableEmails;
+@property (strong, nonatomic) IBOutlet UISwitch *enableTexts;
+
+@end
+
+#endif
---
+++ journey/SettingsPageViewController.m
@@ -0,0 +1,22 @@
+//
+//  SettingsPageViewController.m
+//  journey
+//
+//  Created by Malika Aubakirova on 2/28/15.
+//  Copyright (c) 2015 charles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SettingsPageViewController.h"
+
+@interface SettingsPageViewController ()
+{
+
+}
+@end
+
+@implementation SettingsPageViewController
+
+
+@end
