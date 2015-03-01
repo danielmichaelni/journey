d8779d42820609357eb341c319c68557393d9167
Daniel Ni <danielmichaelni@gmail.com>
2015-02-28 22:52:05 -0600

    working on count down view

--- journey/CountDownViewController.h
+++ journey/CountDownViewController.h
@@ -6,8 +6,15 @@
 //  Copyright (c) 2015 charles. All rights reserved.
 //
 
+#import "Journey.h"
 #import <UIKit/UIKit.h>
 
 @interface CountDownViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *timeLabel;
+
+@property (strong, nonatomic) Journey *journey;
+
+- (IBAction)finishJourneyButton:(UIButton *)sender;
 
 @end
