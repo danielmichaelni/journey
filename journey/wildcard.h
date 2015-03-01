0717e7bca84970745a30c9b0977f2f113ecd6c43
Charles Chamberlain <cchamberlain@uchicago.edu>
2015-02-28 22:18:08 -0600

    Switching to apple maps

---
+++ journey/wildcard.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  journey
+//
+//  Created by Charles Chamberlain on 2/28/15.
+//  Copyright (c) 2015 charles. All rights reserved.
+//
+
+#ifndef journey_Header_h
+#define journey_Header_h
+
+
+#endif
---
+++ journey/wildcard.m
@@ -0,0 +1,9 @@
+//
+//  wildcard.m
+//  journey
+//
+//  Created by Charles Chamberlain on 2/28/15.
+//  Copyright (c) 2015 charles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
