3a300d30152bc1201767ac9ad6d0754558a8a3da
Daniel Ni <danielmichaelni@gmail.com>
2015-05-07 20:36:36 -0500

    does not flash login view

--- journey/LoginViewController.m
+++ journey/LoginViewController.m
@@ -73,6 +73,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    
     // Check if user is cached and linked to Facebook, if so, bypass login
     if ([PFUser currentUser] && [PFFacebookUtils isLinkedWithUser:[PFUser currentUser]]) {
         [self _presentUserDetailsViewControllerAnimated:NO];
